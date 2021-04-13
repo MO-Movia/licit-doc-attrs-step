16cf4fdffc66b8af18dc7eb3b8300b2c0c2cdef4
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2021-04-13 16:48:44 +0530

    To bring UI & Key Command support in plugin.

---
+++ src/KeyCommand.js
@@ -0,0 +1,57 @@
+// @flow
+
+import {Plugin, PluginKey, EditorState} from 'prosemirror-state';
+import Keymap from 'browserkeymap';
+import {keymap} from 'prosemirror-keymap';
+import {Transform} from 'prosemirror-transform';
+import {EditorView} from 'prosemirror-view';
+
+export type UserKeyCommand = (
+  state: EditorState,
+  dispatch: ?(tr: Transform) => void,
+  view: ?EditorView
+) => boolean;
+
+export type UserKeyMap = {
+  [key: string]: UserKeyCommand,
+};
+
+export function makeKeyMap(
+  description: string,
+  windows: string,
+  mac: string,
+  common?: ?string
+): Keymap {
+  return {
+    description: description,
+    windows: windows,
+    mac: mac,
+    common: common,
+  };
+}
+
+export function makeKeyMapWithCommon(
+  description: string,
+  common: string
+): Keymap {
+  const windows = common.replace(/Mod/i, 'Ctrl');
+  const mac = common.replace(/Mod/i, 'Cmd');
+  return makeKeyMap(description, windows, mac, common);
+}
+
+// [FS] IRAD-1005 2020-07-07
+// Upgrade outdated packages.
+// set plugin keys so that to avoid duplicate key error when keys are assigned automatically.
+export function setPluginKey(plugin: Plugin, key: string) {
+  if (plugin && plugin.spec) {
+    plugin.spec.key = new PluginKey(key + 'Plugin');
+    if (plugin.spec.key) {
+      plugin.key = plugin.spec.key.key;
+    }
+  }
+  return plugin;
+}
+
+export function createKeyMapPlugin(pluginKeyMap: UserKeyMap, name: string) {
+  return setPluginKey(keymap(pluginKeyMap), name);
+}
