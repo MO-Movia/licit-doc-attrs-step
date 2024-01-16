48891e590189cb98e6f940193a2bc7066e8362d6
Ryan Culp <rculp@modusoperandi.com>
2024-01-16 11:27:54 -0500

    Patched dependencies removed bable and webpack turned on typescript strict

--- src/UICommand.test.ts
+++ src/UICommand.test.ts
@@ -1,12 +1,32 @@
 import {UICommand} from './UICommand';
 import {EditorState, Transaction} from 'prosemirror-state';
 import {createEditor, doc, p} from 'jest-prosemirror';
-import {EditorView} from 'prosemirror-view';
 import {Node} from 'prosemirror-model';
+import {Transform} from 'prosemirror-transform';
 
+class mockUICommand extends UICommand {
+  renderLabel() {
+    throw new Error('Method not implemented.');
+  }
+  isActive(): boolean {
+    return true;
+  }
+  waitForUserInput(): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+  executeWithUserInput(): boolean {
+    throw new Error('Method not implemented.');
+  }
+  cancel(): void {
+    throw new Error('Method not implemented.');
+  }
+  executeCustom(): Transform {
+    throw new Error('Method not implemented.');
+  }
+}
 describe('UICommand', () => {
   const editor = createEditor(doc(p('<cursor>')));
-  const uiCmd = new UICommand();
+  const uiCmd: UICommand = new mockUICommand();
   beforeAll(() => {
     jest.spyOn(console, 'error').mockImplementation(() => {
       return;
@@ -26,30 +46,6 @@ describe('UICommand', () => {
     expect(respond).toEqual(true);
   });
 
-  it('should execute custom', () => {
-    const tr = uiCmd.executeCustom(editor.state, editor.state.tr, 0, 0);
-    expect(tr.doc).toBe(editor.state.tr.doc);
-  });
-
-  it('should not be active by default', () => {
-    const active = uiCmd.isActive(editor.state);
-    expect(active).toEqual(false);
-  });
-
-  it('should label be null by default', () => {
-    const label = uiCmd.renderLabel(editor.state);
-    expect(label).toEqual(null);
-  });
-
-  it('should be disabled if error thrown', () => {
-    jest.spyOn(uiCmd, 'waitForUserInput').mockRejectedValue('this is error');
-    const enabled = uiCmd.isEnabled(
-      editor.state,
-      editor.view as unknown as EditorView
-    );
-    expect(enabled).toEqual(false);
-  });
-
   describe('dryRunEditorStateProxyGetter', () => {
     let tr: Transaction;
     let state;
@@ -59,7 +55,7 @@ describe('UICommand', () => {
       tr = new Transaction({} as unknown as Node);
       jest.spyOn(tr, 'setMeta').mockImplementation(() => tr);
       state = {tr, other: tr};
-      uiCmd = new UICommand();
+      uiCmd = new mockUICommand();
     });
 
     describe('when getting the transaction', () => {
@@ -79,11 +75,6 @@ describe('UICommand', () => {
     });
   });
 
-  describe('cancel ', () => {
-    it('should be retutn undefined', () => {
-      expect(uiCmd.cancel()).toBeUndefined();
-    });
-  });
   describe('dryRunEditorStateProxySetter', () => {
     it('should set state property', () => {
       const testVal = 'xVal';
@@ -93,15 +84,9 @@ describe('UICommand', () => {
     });
   });
 
-  describe('executeWithUserInput', () => {
-    it('should be return false', () => {
-      const state_ = {} as unknown as EditorState;
-      expect(uiCmd.executeWithUserInput(state_)).toBeFalsy();
-    });
-  });
   describe('execute ', () => {
     it('should execute ', () => {
-      const spy = jest.spyOn(uiCmd,'waitForUserInput').mockResolvedValue({});
+      const spy = jest.spyOn(uiCmd, 'waitForUserInput').mockResolvedValue({});
       const state_ = {} as unknown as EditorState;
       uiCmd.execute(state_);
       expect(spy).toHaveBeenCalled();
